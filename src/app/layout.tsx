f4e3e6c4d96dc7dc17148afe5d6d414e01b57e1c
nehab444 <nehab4@ad.unc.edu>
2024-03-19 22:37:28 -0400

    Commit 5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from 'next/link'; //
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+        <nav>
+  <Link href="/" className="mr-4">Home</Link>
+  <Link href="/AppRouterPage" className="mr-4">App Router</Link>
+  <Link href="/FilePage" className="mr-4">NextJS Files</Link>
+  <Link href="/ServerComponentsPage">Server Components</Link>
+</nav>
+
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
